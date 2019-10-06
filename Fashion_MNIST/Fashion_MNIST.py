c3324e22b4822c2965fb22b214cfb1b1e9b7276f
flybiubiu <flybiubiu@gmail.com>
2019-10-06 17:26:50 +0800

    Fashion

---
+++ Fashion_MNIST/Fashion_MNIST.py
@@ -0,0 +1,8 @@
+import torch
+import torchvision
+import torchvision.transforms as tranforms
+import matplotlib.pylab as plt
+import time
+import sys
+sys.path.append("..")
+import d2lzh_pytorch as d2l
